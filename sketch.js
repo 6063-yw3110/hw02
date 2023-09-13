481e2087971112ec55874ef4b69f36f0a79a3466
luciawav <144282035+luciawav@users.noreply.github.com>
2023-09-13 15:24:12 -0400

    square and ellipse

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
+ createCanvas(1920, 1080);//w,h
+ background(250,0,145);//rgb color
 }
 
 function draw() {
+noStroke();
+ellipse(1920/2, 1080/2, 55, 55,);//x,y,w,h
+fill("orange");
+ellipse(20, 1080/2, 200,200);//x,y,w,h
+fill(20,190,30);
+stroke(12,220,220);
+strokeWeight(4);
+rect(300, 200, 320, 400);//topleft,
 }
